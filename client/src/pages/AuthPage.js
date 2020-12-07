86cf9aa58f64fe502d196d2dff60f521a88e4625
Дмитро Єфременко <mitrandirwhite@MacBook-Pro-Dmitro.local>
2020-12-08 00:40:02 +0200

    heroku-deploy

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -48,7 +48,7 @@ export const AuthPage = () => {
     return (
         <div className="row">
             <div className="col s12 m10 offset-m1 l6 offset-l3 center-align">
-                <h1>Shortlink</h1>
+                <h1>Sl1nk</h1>
                 <div className="card light-blue darken-1">
                     <div className="card-content white-text">
                         <span className="card-title center-align">Авторизація</span>  
