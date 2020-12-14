28a51e6db93a36a8525ba30b277ceba2468a300f
Дмитро Єфременко <mitrandirwhite@MacBook-Pro-Dmitro.local>
2020-12-14 15:21:37 +0200

    buttons mobile center

--- client/src/components/LinksList.js
+++ client/src/components/LinksList.js
@@ -1,39 +1,14 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { useHttp } from '../hooks/http.hook'
-import {useMessage} from '../hooks/message.hook'
-import {AuthContext} from '../context/AuthContext'
 import {Link} from 'react-router-dom'
 
-export const LinksList = ({ links }) => {
+export const LinksList = ({ links, onSort, onDelete, sortDirection }) => {
 
-    const { loading, request } = useHttp()
-    const message = useMessage()
-    const auth = useContext(AuthContext)
-    //const history = useHistory()
-
-    const sortHandler = event => {
-        event.preventDefault()
-    }
+    const { loading } = useHttp()
+    
 
     if (!links.length) {
         return <h5 className="center">У вас поки нема посилань</h5>
-    }
-
-    const deleteHandler = async event => {
-            try {
-                const linkId = event.currentTarget.getAttribute('name')
-                const deleteConfirmed = window.confirm("Видалити посилання?");
-                if (deleteConfirmed) {
-                    await request('/api/link/delete/' + linkId, 'DELETE', {}, {
-                        Authorization: `Bearer ${auth.token}`
-                    })
-                    
-                    message('Видалено скорочене посилання')
-                    window.location.reload()
-                    return
-                }
-                message('Скасовано')
-            } catch (e) {}      
     }
 
     return (
@@ -41,25 +16,55 @@ export const LinksList = ({ links }) => {
             <thead>
             <tr className="linklist_table_top">
                 <th>
-                    №
+                    <button 
+                        className="btn grey lighten-4 black-text waves-effect waves-dark"
+                        onClick={(e)=>{onSort('index', e)}}
+                        >
+                            №
+                            <i className="material-icons prefix table_sort_arrow">arrow_drop_down</i>
+                    </button>
                 </th>
                 <th>
-                    Коротке посилання       
+                    <button 
+                        className="btn grey lighten-4 black-text waves-effect waves-dark"
+                        onClick={(e)=>{onSort('to', e)}}
+                        >
+                            Коротке посилання 
+                            <i className="material-icons prefix table_sort_arrow"></i>   
+                    </button>   
                 </th>
                 <th>
-                    Оригінальне посилання        
+                    <button 
+                        className="btn grey lighten-4 black-text waves-effect waves-dark"
+                        onClick={(e)=>{onSort('from', e)}}
+                        >
+                            Оригінальне посилання  
+                        <i className="material-icons prefix table_sort_arrow"></i>   
+                    </button>         
                 </th>
                 <th>
-                    Управління
+                    <button 
+                        className="btn grey lighten-4 black-text waves-effect waves-dark">
+                            Управління
+                    </button> 
                 </th>
             </tr>
             </thead>
 
             <tbody>
             { links.slice(0).reverse().map((link, index) => {
+                //Sorting reverse for № column
+                switch (sortDirection){
+                    case 'ascending':
+                        index = index+1
+                    break
+                    case 'descending':
+                        index = links.length - index
+                    break
+                 }
                 return (
                 <tr key={link._id}>
-                    <td>{index+1}</td>
+                    <td>{index}</td>
                     <td>
                         <div className="linkslist_oldlink_inner">
                             <a href={link.to} target="_blank" rel="noopener noreferrer" className="old_link_a">{link.to}</a>
@@ -74,8 +79,8 @@ export const LinksList = ({ links }) => {
                         <button className="btn light-blue darken-1 waves-effect waves-light linklist_details">
                             <Link to={`/detail/${link._id}`} className="linklist_details_open">Деталі</Link>
                         </button>
-                        <button className="btn orange lighten-1 waves-effect waves-dark linklist_delete" onClick={deleteHandler} name={link._id}>
-                            {/* <Link to={`/delete/${link._id}`} className="linklist_details_open">Видалити</Link> */}<i className="large material-icons linklist_details_open">delete</i>
+                        <button className="btn orange lighten-1 waves-effect waves-dark linklist_delete" onClick={()=>{onDelete(link._id)}} name={link._id}>
+                            <i className="large material-icons linklist_details_open">delete</i>
                         </button>
                     </td>
                 </tr>
--- client/src/pages/LinksPage.js
+++ client/src/pages/LinksPage.js
@@ -3,11 +3,15 @@ import { AuthContext } from '../context/AuthContext'
 import { useHttp } from '../hooks/http.hook'
 import { Loader } from '../components/Loader'
 import { LinksList } from '../components/LinksList'
+import {useMessage} from '../hooks/message.hook'
 
 export const LinksPage = () => {
     const [links, setLinks] = useState([])
+    const [dbLinks, setDBLinks] = useState([])
+    const [sortDirection, setSortDirection] = useState('ascending')
     const {loading, request} = useHttp()
     const {token} = useContext(AuthContext)
+    const message = useMessage() 
 
     const fetchLinks = useCallback( async () => {
         try {
@@ -15,8 +19,73 @@ export const LinksPage = () => {
                 Authorization: `Bearer ${token}`
             })
             setLinks(fetched)
+            setDBLinks(fetched)
         } catch (e) {}
     }, [token, request])
+
+    const deleteHandler = async linkId => {
+        try {
+            const deleteConfirmed = window.confirm("Видалити посилання?")
+            if (deleteConfirmed) {
+                await request('/api/link/delete/' + linkId, 'DELETE', {}, {
+                    Authorization: `Bearer ${token}`
+                })
+                
+                message('Видалено скорочене посилання')
+                setLinks(links.filter(link => link._id !== linkId))
+                return
+            }
+            message('Скасовано')
+        } catch (e) {}      
+    }
+
+    const sortHandler = async (field, e) => {
+        try {
+            let direction = sortDirection
+            let sortedLinks = [...links]
+            let sortedDBLinks = [...dbLinks]
+
+            const descendingArrow = 'arrow_drop_up'
+            const ascendingArrow = 'arrow_drop_down'
+
+            // Bud' laska ne svarit' za ce
+            document.querySelectorAll('tr.linklist_table_top i').forEach((el)=>{el.innerHTML = ''})
+            // Changing sort direction
+            if(direction !== 'ascending'){
+                direction = 'ascending'
+                e.target.querySelector('i').innerHTML = ascendingArrow
+            }else{
+                direction = 'descending'
+                e.target.querySelector('i').innerHTML = descendingArrow
+            }
+            setSortDirection(direction)
+
+            // If the field (parameter) user sorts by is №
+            if(field === 'index'){
+                switch (sortDirection){
+                    case 'ascending':
+                        sortedDBLinks.sort((link1, link2) => (link1._id < link2._id) ? 1 : -1)
+                    break
+                    case 'descending':
+                        sortedDBLinks.sort((link1, link2) => (link1._id > link2._id) ? 1 : -1)
+                    break
+                }
+                setLinks(sortedDBLinks)
+                return
+            }
+
+            switch (sortDirection){
+                case 'ascending':
+                    sortedLinks.sort((link1, link2) => (link1[field] > link2[field]) ? 1 : -1)
+                break
+                case 'descending':
+                    sortedLinks.sort((link1, link2) => (link1[field] < link2[field]) ? 1 : -1)
+                break
+            }
+            setLinks(sortedLinks)
+            
+        } catch (e) {console.log(e)}      
+    }
 
     useEffect(() => {
         fetchLinks()
@@ -28,7 +97,7 @@ export const LinksPage = () => {
 
     return (
         <>
-            { !loading && <LinksList links={ links }/> }
+            { !loading && <LinksList links={ links } sortDirection={ sortDirection } onSort={ sortHandler } onDelete = { deleteHandler }/> }
         </>
     )
 }
